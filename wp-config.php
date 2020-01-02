bafda710a84983cb6e1116a46e568f832c0c300f
AchudhKrishnaAG <Work@MBP.local>
2020-01-02 14:56:16 +0530

    init docker compose

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'botminds' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'passwords' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
